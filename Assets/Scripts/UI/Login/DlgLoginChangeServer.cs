082458d991fff1977ef3a7e0ff4b9ad72d0ed209
lwj <wwnje@qq.com>
2018-03-28 11:26:09 +0800

    搭建UI

---
+++ Assets/Scripts/UI/Login/DlgLoginChangeServer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DlgLoginChangeServer : MonoBehaviour
+{
+    public ScrollRect PnlChooseList;
+    public ScrollRect PnlDetailList;
+
+    public GameObject ChooseItem;
+    public GameObject DetailItem;
+
+    Transform chooseItemParent;
+    Transform detalItemParent;
+
+    void Start()
+    {
+        chooseItemParent = PnlChooseList.content;
+        detalItemParent = PnlDetailList.content;
+
+        OnOpen();
+    }
+
+    public void OnOpen()
+    {
+        // refresh
+        foreach (Transform child in chooseItemParent)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject item = null;
+            if (i > chooseItemParent.childCount - 1)
+            {
+                item = Instantiate(ChooseItem);
+            }
+            else
+            {
+                item = chooseItemParent.GetChild(i).gameObject;
+            }
+
+            item.transform.parent = chooseItemParent;
+            item.GetComponentInChildren<Text>().text = i + "";
+            item.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
